aaf807e833c626f724481bb46de783ebaaa8f3e6
ronnie1734 <rlewwwis@yahoo.com>
2017-10-10 18:48:01 -0700

    added html boiler plate and created store location objects

---
+++ app.js
@@ -0,0 +1,29 @@
+var firstAndPike = {
+  minCust: 23 ,
+  maxCust: 65 ,
+  avgSales: 6.3 ,
+};
+
+var seaTacAir = {
+  minCust: 3 ,
+  maxCust: 24 ,
+  avgSales: 1.2 ,
+};
+
+var seattleCenter = {
+  minCust: 11 ,
+  maxCust: 38 ,
+  avgSales: 3.7 ,
+};
+
+var capitolHill = {
+  minCust: 20 ,
+  maxCust: 38 ,
+  avgSales: 2.3 ,
+};
+
+var alki = {
+  minCust: 2 ,
+  maxCust: 16 ,
+  avgSales: 4.6 ,
+};
