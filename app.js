f9a94a75d5e644c886a68a7dbe9ae92dae6de258
ronnie1734 <rlewwwis@yahoo.com>
2017-10-10 19:11:20 -0700

    Completed step 2 and added script to html

--- app.js
+++ app.js
@@ -2,28 +2,53 @@ var firstAndPike = {
   minCust: 23 ,
   maxCust: 65 ,
   avgSales: 6.3 ,
+  rndmCstGn: function() {
+    var min = Math.ceil(this.minCust);
+    var max = Math.floor(this.maxCust);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 };
 
 var seaTacAir = {
   minCust: 3 ,
   maxCust: 24 ,
   avgSales: 1.2 ,
+  rndmCstGn: function() {
+    var min = Math.ceil(this.minCust);
+    var max = Math.floor(this.maxCust);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 };
 
 var seattleCenter = {
   minCust: 11 ,
   maxCust: 38 ,
   avgSales: 3.7 ,
+  rndmCstGn: function() {
+    var min = Math.ceil(this.minCust);
+    var max = Math.floor(this.maxCust);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 };
 
 var capitolHill = {
   minCust: 20 ,
   maxCust: 38 ,
   avgSales: 2.3 ,
+  rndmCstGn: function() {
+    var min = Math.ceil(this.minCust);
+    var max = Math.floor(this.maxCust);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 };
 
 var alki = {
   minCust: 2 ,
   maxCust: 16 ,
   avgSales: 4.6 ,
+  rndmCstGn: function() {
+    var min = Math.ceil(this.minCust);
+    var max = Math.floor(this.maxCust);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 };
